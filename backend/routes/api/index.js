5f53bf72b527f7c3f73e7ac43e9daa66efd9fecc
Lydia Kuehnert <lydiakuehnert@gmail.com>
2023-03-20 12:54:48 -0400

    fixed signup

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,9 +5,16 @@ const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
 
+
+
+
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+
+
+
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
